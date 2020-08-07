4f0b944116fa9b75a0e4e020f6c2913e07e1da10
Christos <xristos94sideris@gmail.com>
2020-08-07 16:17:52 +0300

    Created UserRepository interface

---
+++ src/main/java/com/WebApp/CodeStruction/repository/UserRepository.java
@@ -0,0 +1,30 @@
+package com.WebApp.CodeStruction.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Override
+    List<User> findAll();
+
+    List<User> findByRole(String role);
+
+    Optional<User> findById(Long id);
+
+    List<User> findBySsn(String ssn);
+
+    List<User> findByEmail(String email);
+
+    List<User> findBySsnAndEmail(String ssn, String email);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
+
+    User save(User user);
+
+    Optional<User> findBySsnOrEmailOrUsername(String ssn, String email, String  username);
+
+}
