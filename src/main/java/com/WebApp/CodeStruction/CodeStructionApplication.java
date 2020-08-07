cd5f40e9c68c254b20d5b9e5924b0b09a480b2b8
Christos <xristos94sideris@gmail.com>
2020-08-07 14:24:15 +0300

    First commit

---
+++ src/main/java/com/WebApp/CodeStruction/CodeStructionApplication.java
@@ -0,0 +1,13 @@
+package com.WebApp.CodeStruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeStructionApplication.class, args);
+	}
+
+}
