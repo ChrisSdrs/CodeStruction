3beb10828780eea060bc817f048e16adeb775b06
Christos <xristos94sideris@gmail.com>
2020-08-11 14:32:41 +0300

    Imported javax.validation.Valid class in UserCreateController, RepairCreateController, PropertyCreateController classes

--- src/main/java/com/WebApp/CodeStruction/controller/user/UserCreateController.java
+++ src/main/java/com/WebApp/CodeStruction/controller/user/UserCreateController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+
 import static javax.servlet.RequestDispatcher.ERROR_MESSAGE;
 
 @Controller
