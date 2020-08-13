41b07f4a08d4980a41dccb60532f7978aec061ff
Christos <xristos94sideris@gmail.com>
2020-08-13 19:38:09 +0300

    Edited CodeStructionApplication file

--- src/main/java/com/WebApp/CodeStruction/CodeStructionApplication.java
+++ src/main/java/com/WebApp/CodeStruction/CodeStructionApplication.java
@@ -1,13 +1,38 @@
 package com.WebApp.CodeStruction;
 
+import com.WebApp.CodeStruction.controller.repair.RepairController;
+import com.WebApp.CodeStruction.controller.user.UserController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
-public class CodeStructionApplication {
+public class CodeStructionApplication implements CommandLineRunner {
+
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	
+	@Autowired
+	private UserController userController;
+
+	@Autowired
+	private RepairController repairController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeStructionApplication.class, args);
+		System.out.println("Test");
+
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Starting");
+
+
+		System.out.println("Ended");
+	}
+
 
 }
