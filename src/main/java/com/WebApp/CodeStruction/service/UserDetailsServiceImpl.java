bb951eb67475f143244a4a1b882157158923ae5e
Christos <xristos94sideris@gmail.com>
2020-08-14 11:05:07 +0300

    Edited UserDetailsServiceImpl file, now app is usable

--- src/main/java/com/WebApp/CodeStruction/service/UserDetailsServiceImpl.java
+++ src/main/java/com/WebApp/CodeStruction/service/UserDetailsServiceImpl.java
@@ -1,13 +1,32 @@
 package com.WebApp.CodeStruction.service;
 
+import com.WebApp.CodeStruction.domain.User;
+import com.WebApp.CodeStruction.model.LoginResponse;
+import com.WebApp.CodeStruction.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Arrays;
+
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // here we would search into the repo for the user.
+        // for not we are just going to send always a successful response.
+        User user = userRepository.findByUsername(username).get();
+
+/*        List<SimpleGrantedAuthority> authorization = Collections.singletonList(new SimpleGrantedAuthority("ADMIN"));
+        CharSequence password = "password";*/
+
+        return new LoginResponse(user.getUsername(), user.getPassword(), Arrays.asList(new SimpleGrantedAuthority(user.getRole())));
+
+
     }
 }
